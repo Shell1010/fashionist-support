20ce5a479849990c2b2387359774e42d3c496a80
shell1010 <amin.dev03@gmail.com>
2025-06-15 17:45:12 +0100

    stuffs yes

--- src/cogs/char.py
+++ src/cogs/char.py
@@ -16,14 +16,16 @@ class Char(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name="char", description="Generates character based on char page")
-    @app_commands.describe(username="Player to generate")
+    @app_commands.describe(username="Player to generate, doesn't work yet ignore")
     async def test(self, interaction: discord.Interaction, username: str):
-        await interaction.response.defer()
-        page = await self.bot.get_char_page(username)
-        with open("image.html", "a+") as f:
-            f.write(page)
-        await interaction.followup.send("ok")
-
+        if interaction.user.id in self.bot.admins.values():
+            await interaction.response.defer()
+            page = await self.bot.get_char_page(username)
+            with open("image.html", "a+") as f:
+                f.write(page)
+            await interaction.followup.send("ok")
+        else:
+            await interaction.response.send_message(embed=self.bot.base_embed("Unauthorized", "You're too stinky..."))
 
 async def setup(bot: FashionThing):
     await bot.add_cog(Char(bot))
